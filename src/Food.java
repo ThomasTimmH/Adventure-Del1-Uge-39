9086cbf4d059cce26417e9919a67c831a06a4a8a
ThomasTimmH <thha0006@stud.kea.dk>
2024-10-03 15:53:31 +0200

    adventure del 2

---
+++ src/Food.java
@@ -0,0 +1,12 @@
+public class Food extends Item {
+private double healthOrDamage;
+
+public Food(String longname, String name, double healthOrDamage){
+    super(longname,name);
+    this.healthOrDamage=healthOrDamage;
+}
+
+    public double getHealthOrDamage() {
+        return healthOrDamage;
+    }
+}
